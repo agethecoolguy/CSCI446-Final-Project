75a2a231e6b57fc0fab4e5db170472d832da25b2
Austin Schussler <aschussl@mymail.mines.edu>
2018-04-29 06:01:25 -0600

    Fix bug number 9

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var gracefulShutdown;
 
 var dbURI = 'mongodb://localhost/Barter';
-if (process.env.NODE_ENV === 'production') {
-    dbURI = process.env.MONGOLAB_URI;
-}
+// if (process.env.NODE_ENV === 'production') {
+//     dbURI = process.env.MONGOLAB_URI;
+// }
 mongoose.connect(dbURI);
 
 var readLine = require ("readline");
